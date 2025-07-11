44a6c5d5d38103ea9fdb744364e241d3972b3c29
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-11 21:11:19 +0800

    end of this day's session

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, abort
 import frontmatter
 import markdown
 import glob
-from jinja2 import Environment, FileSystemLoader
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+def load_post():
 
     posts = []
 
     for filepath in glob.glob("articles/*.md"):
         post = frontmatter.load(filepath)
+
+        slug = os.path.splitext(os.path.basename(filepath))[0]
         title = post['title']
         date = post['date']
         content_html = markdown.markdown(post.content)
 
-        posts.append({"title": title,
+        posts.append({"slug": slug,
+                    "title": title,
                     "date": date,
-                    "content": content_html
+                    "content_html": content_html
                     })
+        
+        return post
 
+@app.route("/")
+def home():
+    posts = load_post()
     return render_template("index.html", article=posts)
+
+@app.route("/article/<string:get_slug>")
+def article_detail(get_slug):
+    all_posts = load_post()
+    found_post = None
+    for post in all_posts:
+        if post['slug'] == get_slug:
+            found_post = post
+            break
+
+    if found_post:
+        return render_template("article_detail.html", post=found_post)
+    else:
+        abort(404)
 
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
